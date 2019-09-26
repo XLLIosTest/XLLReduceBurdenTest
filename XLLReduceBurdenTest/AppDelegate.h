a0c6c189b31d4fa6cc5ba55ec5b9d5be9173f3cd
肖乐 <lele.xiao@melot.cn>
2019-09-26 19:58:04 +0800

    Add project file

---
+++ XLLReduceBurdenTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XLLReduceBurdenTest
+//
+//  Created by xiaoll on 2019/9/26.
+//  Copyright © 2019 xiaoll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ XLLReduceBurdenTest/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  XLLReduceBurdenTest
+//
+//  Created by xiaoll on 2019/9/26.
+//  Copyright © 2019 xiaoll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
